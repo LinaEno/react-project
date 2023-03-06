b320677641dc37d2f3c196bd5ef6cc127e33c4a4
Anastasiia Klyosova <112250768+LeniFraud@users.noreply.github.com>
2023-03-06 23:04:07 +0200

    added mobile modal logic, added modal icons

--- src/components/ButtonAddTransactions/ButtonAddTransactions.styled.js
+++ src/components/ButtonAddTransactions/ButtonAddTransactions.styled.js
@@ -1,23 +1,31 @@
 import styled from 'styled-components';
+import { ReactComponent as AddIcon } from '../../images/svg/add.svg';
 
 export const Button = styled.button`
   position: fixed;
-  bottom: 40px;
-  right: 40px;
+  bottom: 20px;
+  right: 20px;
   z-index: 50;
   justify-content: center;
   align-items: center;
   width: 44px;
   height: 44px;
-  color: #24cca7;
-  background-color: transparent;
+  color: #fff;
+  background-color: #24cca7;
   border: none;
   border-radius: 50%;
+  box-shadow: 0px 6px 15px rgba(36, 204, 167, 0.5);
   outline: none;
   cursor: pointer;
-  /* transition: color 250ms linear; */
-  /* &:hover,
-  &:focus {
-    color: #df3e89;
-  } */
+
+  @media screen and (min-width: 768px) {
+    bottom: 40px;
+    right: 40px;
+  }
+`;
+
+export const Icon = styled(AddIcon)`
+  width: 20px;
+  height: 20px;
+  stroke: currentColor;
 `;
