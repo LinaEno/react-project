fb3877a9c97998fe9783f20a57b428e4696ea0db
Svetlana-Dehtiarova <degsvet1977@gmail.com>
2023-03-06 21:28:18 +0200

    Add Header

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,6 +1,6 @@
 import Balance from 'components/Balance/Balance';
 import { ButtonAddTransactions } from 'components/ButtonAddTransactions/ButtonAddTransactions';
-import { ButtonLogout } from 'components/ButtonLogout/ButtonLogout';
+import { Header } from 'components/Header/Header';
 import { ModalContainer } from 'components/ModalContainer/ModalContainer';
 import ModalLogout from 'components/ModalLogout/ModalLogout';
 import Navigation from 'components/Navigation/Navigation';
@@ -16,8 +16,7 @@ const DashboardPage = () => {
 
   return (
     <div>
-      Hello
-      <ButtonLogout />
+      <Header />
       {isModalLogoutOpen && (
         <ModalContainer>
           <ModalLogout />
