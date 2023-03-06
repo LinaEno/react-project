a271d72b37ae36d7a0b501e0b6cdecc90757510b
Anastasiia Klyosova <112250768+LeniFraud@users.noreply.github.com>
2023-03-06 16:43:54 +0200

    added modal container and open buttons

---
+++ src/components/ModalContainer/ModalContainer.jsx
@@ -0,0 +1,42 @@
+import { MdClose } from 'react-icons/md';
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import { Overlay, ModalBox, CloseButton } from './ModalContainer.styled';
+import { useDispatch } from 'react-redux';
+import { closeModal } from 'redux/global/slice';
+
+const modalRoot = document.getElementById('modal-root');
+
+export const ModalContainer = ({ children }) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const onEscapeClick = e => {
+      if (e.code === 'Escape') dispatch(closeModal());
+    };
+
+    //   document.body.style = 'overflow-y: hidden';
+    window.addEventListener('keydown', onEscapeClick);
+
+    return () => {
+      // document.body.style = 'overflow-y: auto';
+      window.removeEventListener('keydown', onEscapeClick);
+    };
+  }, [dispatch]);
+
+  const onOverlayClick = e => {
+    if (e.target === e.currentTarget) dispatch(closeModal());
+  };
+
+  return createPortal(
+    <Overlay onClick={onOverlayClick}>
+      <ModalBox>
+        <CloseButton type="button" onClick={() => dispatch(closeModal())}>
+          <MdClose size={28} />
+        </CloseButton>
+        {children}
+      </ModalBox>
+    </Overlay>,
+    modalRoot
+  );
+};
--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,7 +1,34 @@
+import { ButtonAddTransactions } from 'components/ButtonAddTransactions/ButtonAddTransactions';
+import { ButtonLogout } from 'components/ButtonLogout/ButtonLogout';
+import { ModalContainer } from 'components/ModalContainer/ModalContainer';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import {
+  selectLogoutModalOpen,
+  selectModalAddTransactionOpen,
+} from 'redux/global/selectors';
 
 const DashboardPage = () => {
-  return <div>Hello</div>;
+  const isModalAddTransactionOpen = useSelector(selectModalAddTransactionOpen);
+  const isModalLogoutOpen = useSelector(selectLogoutModalOpen);
+
+  return (
+    <div>
+      Hello
+      <ButtonLogout />
+      {isModalLogoutOpen && (
+        <ModalContainer>
+          <div>Logout Info</div>
+        </ModalContainer>
+      )}
+      <ButtonAddTransactions />
+      {isModalAddTransactionOpen && (
+        <ModalContainer>
+          <div>Add Transaction Info</div>
+        </ModalContainer>
+      )}
+    </div>
+  );
 };
 
 export default DashboardPage;
