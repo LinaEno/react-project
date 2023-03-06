8dfc29dc9651b32a82c17c9d2fdb67f59b5e9fd7
darinadari13 <92116950+darinadari13@users.noreply.github.com>
2023-03-06 21:44:25 +0200

    added NEW markup

--- src/components/TransactionsList/TransactionsList.jsx
+++ src/components/TransactionsList/TransactionsList.jsx
@@ -1,89 +1,84 @@
 
-import React from "react";
+import React, { useEffect } from "react";
 import { ReactComponent as EditIcon } from 'images/Icon.svg';
 import { Table } from "./TransactionsList.styled";
+import { fetchTransactions, fetchCategories } from '../../redux/transactions/operations';
+import { useSelector, useDispatch } from "react-redux";
+import { selectTransactionsWithCategories } from '../../redux/transactions/selectors';
+
+const columns = [
+  {
+    title: 'Date',
+    key: 'date'
+  },
+  {
+    title: 'Type',
+    key: 'type'
+  },
+  {
+    title: 'Category',
+    key: 'category'
+  },
+  {
+    title: 'Comment',
+    key: 'comment'
+  },
+  {
+    title: 'Sum',
+    key: 'sum'
+  },
+  {
+    title: '',
+    key: 'actions'
+  },
+]
+
+const TRANSACTIONS_MOCK = [
+  {
+    "id": "1",
+    "transactionDate": "03.04.2022",
+    "type": "INCOME",
+    "categoryId": "5",
+    "userId": "string",
+    "comment": "Gift for your wife",
+    "amount": 0,
+    "balanceAfter": 0
+  }
+]
 
 
-export function TransactionsList () {
+export function TransactionsList ({ transaction = TRANSACTIONS_MOCK }) {
+  const dispatch = useDispatch();
+  const transactions = useSelector(selectTransactionsWithCategories);
+  
+  
+  useEffect(() => {
+    dispatch(fetchTransactions())
+    dispatch(fetchCategories())
+  }, [dispatch]);
+  
+
   return (
       <Table>
         <thead>
           <tr>
-            <th>Date</th>
-            <th>Type</th>
-            <th>Category</th>
-            <th>Comment</th>
-            <th>Sum</th>
-            <th></th>
-            <th></th>
+            {columns.map(col => <th key={col.key}>{col.key}</th>)}
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>04.01.19</td>
-            <td>-</td>
-            <td>Other</td>
-            <td>Gift for your wife</td>
-            <td>300.00</td>
-            <td>
-              <button className="editButton"><EditIcon/></button>
-            </td>
-            <td>
-              <button>Delete</button>
-            </td>
-          </tr>
-          <tr>
-            <td>04.01.19</td>
-            <td>-</td>
-            <td>Other</td>
-            <td>Gift for your wife</td>
-            <td>300.00</td>
-            <td>
-              <button className="editButton"><EditIcon/></button>
-            </td>
-            <td>
-              <button>Delete</button>
-            </td>
-          </tr>
-          <tr>
-            <td>04.01.19</td>
-            <td>-</td>
-            <td>Other</td>
-            <td>Gift for your wife</td>
-            <td>300.00</td>
-            <td>
-              <button className="editButton"><EditIcon/></button>
-            </td>
-            <td>
-              <button>Delete</button>
-            </td>
-          </tr>
-          <tr>
-            <td>04.01.19</td>
-            <td>-</td>
-            <td>Other</td>
-            <td>Gift for your wife</td>
-            <td>300.00</td>
-            <td>
-              <button className="editButton"><EditIcon/></button>
-            </td>
-            <td>
-              <button>Delete</button>
-            </td>
-          </tr>
-          <tr>
-            <td>04.01.19</td>
-            <td>-</td>
-            <td>Other</td>
-            <td>Gift for your wife</td>
-            <td>300.00</td>
-            <td>
-              <button className="editButton"><EditIcon/></button>
-            </td>
-            <td>
-              <button>Delete</button>
-            </td>
-          </tr>
+          {transaction.map(({transaction}) => (
+            <tr key={transaction.id}>
+              <td>{transaction.transactionDate}</td>
+              <td>{transaction.type === 'INCOME' ? '-' : '+'}</td>
+              <td>{transaction.category.name}</td>
+              <td>{transaction.comment}</td>
+              <td>{transaction.amount}</td>
+              <td>
+                <button className="editButton"><EditIcon/></button>
+                <button>Delete</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </Table>
   );
