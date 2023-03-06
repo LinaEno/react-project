1449149eb7eda83dfbf7811a41996abb26a06407
Anna Atiskova <anna.atiskova@gmail.com>
2023-03-06 17:31:43 +0200

    skeleton Modal Logout

---
+++ src/components/ModalLogout/ModalLogout.jsx
@@ -0,0 +1,25 @@
+import { useDispatch } from 'react-redux';
+import { logOut } from 'redux/auth/authOperation';
+import { closeModalLogout } from 'redux/global/slice';
+
+const ModalLogout = () => {
+  const dispatch = useDispatch();
+
+  const closeModal = () => dispatch(closeModalLogout());
+
+  return (
+    <div>
+      <p>Are you sure you want to Logout</p>
+      <div>
+        <button type="button" onClick={closeModal}>
+          Yes
+        </button>
+        <button type="button" onClick={() => dispatch(logOut());}>
+          No
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ModalLogout;
