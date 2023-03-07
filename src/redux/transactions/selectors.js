57e0c7fab745107e24290c302c5b45dd094ae599
Anastasiia Klyosova <112250768+LeniFraud@users.noreply.github.com>
2023-03-08 00:12:40 +0200

    added pagination component

--- src/redux/transactions/selectors.js
+++ src/redux/transactions/selectors.js
@@ -1,14 +1,15 @@
 export const selectSummary = state => state.transactions.summary;
 export const selectCategories = state => state.transactions.categories;
 export const selectTransactions = state => state.transactions.transactions;
+export const selectPage = state => state.transactions.page;
+export const selectPerPage = state => state.transactions.perPage;
 export const selectIsLoading = state => state.transactions.isLoading;
 export const selectError = state => state.transactions.error;
 export const selectBalance = state => state.transactions.balance;
 export const selectTransactionsWithCategories = state => {
-  const categories = selectCategories(state)
+  const categories = selectCategories(state);
   return selectTransactions(state).map(transaction => ({
     ...transaction,
-    category: categories.find(c => c.id === transaction.categoryId)
-  }))
+    category: categories.find(c => c.id === transaction.categoryId),
+  }));
 };
-
