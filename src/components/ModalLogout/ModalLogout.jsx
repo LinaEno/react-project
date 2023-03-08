dcc443d11c1e4cb1d48a3883a0438f705f30b3af
Anna Atiskova <anna.atiskova@gmail.com>
2023-03-08 02:22:04 +0200

    add responsive to Media

--- src/components/ModalLogout/ModalLogout.jsx
+++ src/components/ModalLogout/ModalLogout.jsx
@@ -4,6 +4,7 @@ import { FcQuestions } from 'react-icons/fc';
 import { logOut } from 'redux/auth/authOperation';
 import { closeModalLogout } from 'redux/global/slice';
 import { Wrapper, Content, Question, ButtonsList, Button } from './ModalLogout.styled';
+import { Default } from '../Media/Media'
 
 
 const ModalLogout = () => {
@@ -15,10 +16,10 @@ const ModalLogout = () => {
     <Wrapper>
       <Question>Are you sure you want to Logout?</Question>
       <Content>
-      <FcQuestions size="144px" />
+        <Default><FcQuestions size="144px" /></Default>
         <ButtonsList>
           <Button type="button" onClick={() => dispatch(logOut())}>
-            <NavLink to={'/login'}>Yes</NavLink>
+            <NavLink styled={{TextDecoder: 'none'}} to={'/login'}>Yes</NavLink>
           </Button>
           <Button type="button" onClick={closeModal}>
             No
