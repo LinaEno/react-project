0847c1df83a36110107d971cf8b1b90dfaa7eea9
HannaMois <hannamois135@gmail.com>
2023-03-08 10:21:53 +0200

    Create SummaryPage

---
+++ src/pages/SummaryPage/SummaryPage.styled.jsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const StatisticTitle = styled.h2`
+  font-weight: 400;
+  font-size: 30px;
+  line-height: 45px;
+  margin-bottom: 20px;
+`;
+
+export const Wrap = styled.div`
+  display: flex;
+  gap: 32px;
+  width: 715px;
+  height: auto;
+`;
